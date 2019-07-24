f1f8d86e259bd3071b579c98a10d40e5370c4d90
Anna McPherran <annamcpherran@gmail.com>
2019-07-24 13:23:30 +0000

    updated with homework

--- worksheet-4.R
+++ worksheet-4.R
@@ -115,3 +115,54 @@ cbp <- cbp %>%
 
 acs_cbp <- cbp %>%
   inner_join(acs)
+
+
+######################################
+##Anna McPherran
+##July 23, 2019
+##Lesson 4 exercises
+
+##Exercise 1
+##Now that we have a tidy form of survey, convert it to a long_survey data frame using gather. 
+##The only difference between survey and long_survey should be an additional row for zero income.
+library(tidyr)
+long_survey <- gather(tidy_survey,
+                     key = "attr",
+                     value = "val",
+                     -participant)
+long_survey
+
+##Exercise 2
+##Use filter and select to return just the annual payroll data for the top level 
+##construction sector (“23----“).
+original_cbp <- fread('data/cbp15co.csv')
+cbp23_filter <- filter(original_cbp, NAICS == '23----')
+cbp23_select <- cbp23_filter %>% select(starts_with('AP'))
+
+##Exercise 3
+##Write code to create a data frame giving, for each state, the number of counties in the 
+##CBP survey with establishements in mining or oil and gas extraction (‘21—-‘) along with their 
+##total employment (“EMP”). Group the data using both FIPSTATE and FIPSCTY and use the fact that 
+##one call to summarize only combines across the lowest level of grouping. The dplyr function n 
+##counts rows in a group.
+
+cbp21 <- original_cbp %>%
+  filter(NAICS == '21----') %>%
+  group_by(FIPSTATE, FIPSCTY) %>%
+  summarize(EMP = sum(EMP)) %>%
+  summarize(EMP = sum(EMP), counties = n())
+head(cbp21)
+
+##Exercise 4
+##A “pivot table” is a transformation of tidy data into a wide summary table. First, data are 
+##summarized by two grouping factors, then one of these is “pivoted” into columns. Starting 
+##from a filtered CBP data file, chain a split-apply-combine procedure into the tidyr function 
+##spread to get the total number of employees (“EMP”) in each state (as rows) by 2-digit NAICS 
+##code (as columns).
+
+pivot_table <- original_cbp %>%
+  filter(NAICS == '21----') %>%
+  group_by(FIPSTATE, NAICS) %>%
+  summarize(EMP = sum(EMP)) %>%
+  spread(key = NAICS, value = EMP)
+head(pivot_table)
